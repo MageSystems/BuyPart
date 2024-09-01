f03b489c8ab9b827c01829ff40ae5fc7af3b282e
galillei <artsem.belvg@gmail.com>
2024-09-01 11:59:40 +0200

    [BL-5]: add skeleton for tests.

---
+++ Test/Integration/Plugin/ChangeQuoteItemCollectionTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Vinduesgrossisten
+ * @author    artsem.belvg@gmail.com
+ * @copyraight Copyright © 2015 - 2024
+ */
+declare(strict_types=1);
+
+namespace MageSystems\BuyPart\Test\Integration\Plugin;
+
+use PHPUnit\Framework\TestCase;
+
+class ChangeQuoteItemCollectionTest extends TestCase
+{
+    public function testChangeQuoteItemCollection()
+    {
+        $this->assertTrue(false);
+    }
+}
